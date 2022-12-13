edf193aa2702d44227169629d96660ee931c71f2
JeongBokyeong <jeong344377@gmail.com>
2022-12-13 10:24:20 +0900

    Updates

---
+++ precache-manifest.e1c9dc9ff0f2e69897c45a17c1c3a011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1964a61ec062670ae643",
+    "url": "/static/css/main.2deb6389.chunk.css"
+  },
+  {
+    "revision": "1964a61ec062670ae643",
+    "url": "/static/js/main.1964a61e.chunk.js"
+  },
+  {
+    "revision": "5a80405e8fff7c81f25f",
+    "url": "/static/js/1.5a80405e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c6318d30eff69a92ac47a673f246c49",
+    "url": "/static/media/logo.5c6318d3.svg"
+  },
+  {
+    "revision": "b4084e44bea589ec3800fdfdd69cf189",
+    "url": "/static/media/icon-check.b4084e44.svg"
+  },
+  {
+    "revision": "dd641f8f8d1add1274e4b6de04387223",
+    "url": "/index.html"
+  }
+];
