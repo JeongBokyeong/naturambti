54a1b4ee39edc729937135356dc13a97b26ff4bd
JeongBokyeong <jeong344377@gmail.com>
2022-12-13 10:16:30 +0900

    first commit

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,89 +1,161 @@
 var quizQuestions = [
   {
-      question: "What franchise would you rather play a game from?",
+      question: "누군가가 실수를 했을 때 뭐라 말해줄까",
       answers: [
           {
-              type: "Microsoft",
-              content: "Halo"
+              type: "T",
+              content: "상황을 정확히 분석하여 앞으로는 그러지 않게 도와준다."
           },
           {
-              type: "Nintendo",
-              content: "Pokemon"
-          },
-          {
-              type: "Sony",
-              content: "Uncharted"
+              type: "F",
+              content: "공감을 통해 위로해준다."
           }
       ]
   },
   {
-      question: "Which console would you prefer to play with friends?",
+      question: "나의 수다 떠는 스타일은?",
       answers: [
           {
-              type: "Microsoft",
-              content: "X-Box"
+              type: "E",
+              content: "음식 하나를 주문하고 왁자지껄 수다"
           },
           {
-              type: "Nintendo",
-              content: "Nintendo 64"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 1"
+              type: "I",
+              content: "조용한 카페에서 커피 한잔 마시며 수다"
           }
       ]
   },
   {
-      question: "Which of these racing franchises would you prefer to play a game from?",
+      question: "10년만에 친구한테 뭐하냐고 연락이 왔다. 나의 속마음은?",
       answers: [
           {
-              type: "Microsoft",
-              content: "Forza"
+              type: "I",
+              content: "(뭐지..?) 우와 오랜만이야!"
           },
           {
-              type: "Nintendo",
-              content: "Mario Kart"
-          },
-          {
-              type: "Sony",
-              content: "Gran Turismo"
+              type: "E",
+              content: "(어머 웬일이야..!) 우와 오랜만이야!"
           }
       ]
   },
   {
-      question: "Which of these games do you think is best?",
+      question: "자고 일어났더니 낯선 환경이 펼쳐져있다.",
       answers: [
           {
-              type: "Microsoft",
-              content: "BioShock"
+              type: "N",
+              content: "이렇게 된거 그냥 이 상황을 즐겨본다."
           },
           {
-              type: "Nintendo",
-              content: "The Legend of Zelda: Ocarina of Time"
-          },
-          {
-              type: "Sony",
-              content: "Final Fantasy VII"
+              type: "S",
+              content: "즐기기보단 앞으로의 상황을 생각해 살 수 있는 궁리를 한다."
           }
       ]
   },
   {
-      question: "What console would you prefer to own?",
+    question: "오늘 친한 친구와 싸웠다. 자기 전의 행동은?",
+    answers: [
+        {
+            type: "T",
+            content: "졸려.. 일단 자고 내일 일어나서 다시 생각해봐야지.."
+        },
+        {
+            type: "F",
+            content: "잠들기 전까지 생각하다 잠든다."
+        }
+    ]
+},
+  {
+      question: "내가 멍때릴 때는?",
       answers: [
           {
-              type: "Microsoft",
-              content: "X-Box One"
+              type: "S",
+              content: "아무런 생각도 하지 않는다."
           },
           {
-              type: "Nintendo",
-              content: "Wii U"
-          },
-          {
-              type: "Sony",
-              content: "Playstation 4"
+              type: "N",
+              content: "이런저런 상상을 한다."
           }
       ]
-  }
+  },
+  {
+    question: "내가 더욱 중요하게 생각하는 것은?",
+    answers: [
+        {
+            type: "F",
+            content: "일이 수행된 방식과 그 과정이 중요하다."
+        },
+        {
+            type: "N",
+            content: "임무의 완수 여부와 결과가 중요하다."
+        }
+    ]
+},
+{
+    question: "주말 약속이 꽉 찼을 때",
+    answers: [
+        {
+            type: "E",
+            content: "이번 주말도 알차게 보내겠군!  신난다!"
+        },
+        {
+            type: "I",
+            content: " 주말 중 최소 하루는 집이어야하는데... 어쩌지..."
+        }
+    ]
+},
+{
+    question: "내가 여행을 계획할 때는?",
+    answers: [
+        {
+            type: "P",
+            content: "비행기표를 끊었으면 계획의 70%는 끝났다고 생각한다."
+        },
+        {
+            type: "J",
+            content: "일자별로 세부 일정을 꼼꼼하게 정리해둔다."
+        }
+    ]
+},
+{
+    question: "어질러진 집, 뒷정리는 언제 할까?",
+    answers: [
+        {
+            type: "P",
+            content: "힘들지만 치우고 푹 쉬자!"
+        },
+        {
+            type: "J",
+            content: "내일의 나에게 맞기자!"
+        }
+    ]
+},
+{
+    question: "오랜만에 떠나는 여행, 어디로 갈까?",
+    answers: [
+        {
+            type: "P",
+            content: "수많은 리뷰로 검증괸 장소로 가자!"
+        },
+        {
+            type: "J",
+            content: "아무도 안가본 새로운 장소로 떠나자!"
+        }
+    ]
+},
+{
+    question: "화려한 건축물을 보며 드는 생각은?",
+    answers: [
+        {
+            type: "N",
+            content: "어떤 방법으로 지었을까?"
+        },
+        {
+            type: "S",
+            content: "와.. 멋있다.."
+        }
+    ]
+}
+
 ];
 
 export default quizQuestions;
--- src/components/Result.js
+++ src/components/Result.js
@@ -14,7 +14,7 @@ function Result(props) {
       transitionAppearTimeout={500}
     >
       <div>
-        You prefer <strong>{props.quizResult}</strong>!
+        YOU ARE <strong>{props.quizResult}</strong>!
       </div>
     </CSSTransitionGroup>
   );
